b2f9f45b89c970ea8cb8fd43e4053097f4c53180
monoShinobu <100402022+monoShinobu@users.noreply.github.com>
2023-11-11 11:53:54 -0300

    Update Gorila.java

--- clase3/tutoria3/src/tutoria3/ejercicio1/Gorila.java
+++ clase3/tutoria3/src/tutoria3/ejercicio1/Gorila.java
@@ -9,16 +9,20 @@ package tutoria3.ejercicio1;
  * @author puras
  */
 public class Gorila extends Animal{
-    public String sonido = "Gruñe";
     public Gorila(int codigo){
-        super(codigo, 0);
+        super(codigo, 0, "Gorila");
     }
     
     public void agregarCantidad(int cantidad){
-        agregarAnimales(cantidad);
+        super.agregarAnimales(cantidad);
     }
     
     public String sonido(){
-        return super.hacerSonido() + this.sonido;
+        return super.hacerSonido();
+    }
+    
+    @Override
+    public String toString() {
+        return "La cantidad de gorilas es " + this.cantidad;
     }
 }
